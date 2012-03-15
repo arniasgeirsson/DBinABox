f886d823358a1ff4368940460af9fcf501cbde83
unknown <Peter@Peter-Laptop.(none)>
2012-03-15 15:41:00 +0100

    Basis struktur

---
+++ kode/Admin tool/src/model/Backup.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Backup
+{
+    public void autoBackUp()
+    {
+        //TODO
+    }
+    
+    public boolean manuelBackUp()
+    {
+        //TODO
+        return false;
+    }
+    
+    public boolean pushImage()
+    {
+        //TODO
+        return false;
+    }
+}
---
+++ kode/Admin tool/src/model/Chooser.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Chooser
+{
+    public void backup()
+    {
+        //TODO
+    }
+    
+    public void edit()
+    {
+        //TODO
+    }
+}
